8adfb0d0cf55a7c58c470afc2107b333d54ed44c
emrecimen20 <emrecimen48@outlook.com>
2024-05-09 22:56:08 +0300

    The latest version of the minesweeper game added.

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,30 @@ public class Main {
 
         // I used "Scanner" to get the matrix size from the user.
         Scanner input=new Scanner(System.in);
+        int rows,cols;
+
         System.out.println("==Welcome to MineSweeper==");
-        System.out.println("Enter the number of rows and columns to determine the difficulty");
 
-        System.out.print("Enter the number of rows :");
-        int rows=input.nextInt();
-        System.out.print("Enter the number of columns :");
-        int cols=input.nextInt();
+        do {
+            System.out.println("Enter the number of rows and columns to determine the difficulty.");
 
-        // I stipulated that it should have a size of 2x2 and above.
+            System.out.print("Enter the number of rows :");
+            rows=input.nextInt();
+            System.out.print("Enter the number of columns :");
+             cols=input.nextInt();
+            if(rows<2 || cols<2){
+
+                //I stipulated that it should have a size of 2x2 and above.
+                System.out.println("Minimum size allowed is 2x2. Please try again.");
+            }
+        }while (rows<2 || cols<2);
+
         if(rows<2 || cols<2){
-            System.out.println("Minimum size allowed is 2x2. Please try again.");
-            return;
+
         }
 
         // I created an object called "game" and sent the row and column values entered by the user.
         MineSweeper game=new MineSweeper(rows,cols);
-        game.printBoard(game.mineBoard);
+        game.play();
     }
 }
--- src/MineSweeper.java
+++ src/MineSweeper.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class MineSweeper {
 
@@ -17,8 +18,11 @@ public class MineSweeper {
         this.nonMine=(rows * cols)-this.totalMines;// I calculated the number of mine-free places.
         this.gameBoard=new char[rows][cols];
         this.mineBoard=new char[rows][cols];
+        initializeBoard();
+        placeMines();
     }
 
+    // Method that prints my matrix.
     public void printBoard(char[][] board) {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -45,8 +49,8 @@ public class MineSweeper {
 
         // The loop will continue until the total number of mines.
         while (minesPlaced<totalMines){
-            int randRow=rand.nextInt();
-            int randCol=rand.nextInt();
+            int randRow=rand.nextInt(rows);
+            int randCol=rand.nextInt(cols);
             if(mineBoard[randRow][randCol]!='*'){
                 mineBoard[randRow][randCol]='*';
                 minesPlaced++;
@@ -54,9 +58,66 @@ public class MineSweeper {
         }
     }
 
+    // Method that checks the coordinates entered by the user.
     public boolean isValidMove(int row,int col){
         return row>=0 && row<rows && col>=0 && col<cols && gameBoard[row][col]=='-';
     }
 
+
+    public void show(int row,int col){
+        if(!isValidMove(row,col)) {
+            return;
+        }
+
+        // If the user enters the coordinates of the mined cell, the game ends and the mined board is shown to the user.
+        if(mineBoard[row][col]=='*'){
+            System.out.println("Game Over ! You stepped on a mine!");
+            printMineBoard();
+            System.exit(0);
+        }
+
+        // When a cell without a mine is selected
+        int minesAround =countMinesAround(row,col);
+        gameBoard[row][col]=(char) (minesAround+'0');
+        nonMine--;
+    }
+
+    // Method that calculates the number of mines around the selected cell.
+    public int countMinesAround(int row,int col){
+        int count=0;
+        for(int i=row-1;i<=row+1;i++){
+            for(int j=col-1;j<=col+1;j++){
+                if(i>=0 && i<rows && j>=0 && j<cols && mineBoard[i][j]=='*'){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public void printMineBoard(){
+        printBoard(mineBoard);
+    }
+
+    public void play(){
+        Scanner input=new Scanner(System.in);
+        printBoard(gameBoard);
+        while (nonMine>0){
+            System.out.print("Enter row :");
+            int row=input.nextInt();
+            System.out.print("Enter column :");
+            int col=input.nextInt();
+
+            // If the user enters the wrong code and enters the same coordinates, a warning is given.
+            if(!isValidMove(row,col)){
+                System.out.println("Invalid move !! Please enter valid row and column.");
+                continue;
+            }
+            show(row,col);
+            printBoard(gameBoard);
+        }
+        System.out.println("You Win !! You opened all safe cells!");
+
+    }
 
 }
